3a07767d59db25f38497aa46edaf0e12af766f53
YOUR NAME <YOUR KONATE.ABAS@HOTMAIL.FR>
2023-02-06 14:34:12 +0100

    NewUpdate

---
+++ assets/scriptPortfolio.js
@@ -0,0 +1,24 @@
+// const curriculum = document.querySelector('.allId');
+// const background = document.querySelector('.backT');
+
+// curriculum.addEventListener('mouseover', (event) => {
+//         background.style.backgroundColor = '#000000EB';
+//         background.style.visibility = 'visible';
+// });
+
+
+// curriculum.addEventListener('mouseout', (event) => {
+//         background.style.backgroundColor = '';
+//         background.style.visibility = 'hidden';
+// });
+
+const logoFixScroll = document.querySelector('allLogo');
+
+logoFixScroll.addEventListener('scroll', (e) => {
+        logoFixScroll.style.position = 'fixed';
+});
+
+console.log('salut');
+
+
+
