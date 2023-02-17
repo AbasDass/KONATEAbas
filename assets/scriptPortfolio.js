91cbf09877784ac2fd738e05b5cd62039d75c80a
YOUR NAME <YOUR KONATE.ABAS@HOTMAIL.FR>
2023-02-17 14:12:37 +0100

    v5

--- assets/scriptPortfolio.js
+++ assets/scriptPortfolio.js
@@ -1,16 +1,3 @@
-// const curriculum = document.querySelector('.allId');
-// const background = document.querySelector('.backT');
-
-// curriculum.addEventListener('mouseover', (event) => {
-//         background.style.backgroundColor = '#000000EB';
-//         background.style.visibility = 'visible';
-// });
-
-
-// curriculum.addEventListener('mouseout', (event) => {
-//         background.style.backgroundColor = '';
-//         background.style.visibility = 'hidden';
-// });
 
 
 
